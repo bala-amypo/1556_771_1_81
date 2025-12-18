c65e9e28364e5cb710edf3dd66e02f1e7527dff1
bala-amypo <bala@amypo.in>
2025-12-18 16:38:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Implementation/LifecycleEventServiceimpl.java
@@ -0,0 +1,29 @@
+
+@Service
+public class LifecycleEventServiceImpl implements LifecycleEventService {
+
+    @Autowired
+    private LifecycleEventRepository eventRepository;
+
+    @Override
+    public void logEvent(Long assetId, Long userId, LifecycleEvent event) {
+        
+        event.setAssetId(assetId);
+        event.setUserId(userId);
+        event.setTimestamp(LocalDateTime.now());
+        eventRepository.save(event);
+    }
+
+    @Override
+    public List<LifecycleEvent> getEventsForAsset(Long assetId) {
+        
+        return eventRepository.findByAssetId(assetId);
+    }
+
+    @Override
+    public LifecycleEvent getEvent(Long id) {
+        
+        return eventRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Event not found with id: " + id));
+    }
+}
