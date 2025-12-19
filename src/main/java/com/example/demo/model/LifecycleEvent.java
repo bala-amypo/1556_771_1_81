24834402b93765e78c3d04b1ac93cf1f74a3f0e5
bala-amypo <bala@amypo.in>
2025-12-19 09:30:14 +0000

    User commit

--- src/main/java/com/example/demo/model/LifecycleEvent.java
+++ src/main/java/com/example/demo/model/LifecycleEvent.java
@@ -1,7 +1,15 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "lifecycle_events")
@@ -37,6 +45,14 @@ public class LifecycleEvent {
         this.performedBy = performedBy;
     }
 
+    public LifecycleEvent(Asset asset, LocalDateTime eventDate, String eventDescription, String eventType, User performedBy) {
+        this.asset = asset;
+        this.eventDate = eventDate;
+        this.eventDescription = eventDescription;
+        this.eventType = eventType;
+        this.performedBy = performedBy;
+    }
+
     @PrePersist
     public void prePersist() {
         if (eventDate == null) {
@@ -44,5 +60,53 @@ public class LifecycleEvent {
         }
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public Asset getAsset() {
+        return asset;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    public LocalDateTime getEventDate() {
+        return eventDate;
+    }
+
+    public User getPerformedBy() {
+        return performedBy;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAsset(Asset asset) {
+        this.asset = asset;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
+    }
+
+    public void setEventDate(LocalDateTime eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public void setPerformedBy(User performedBy) {
+        this.performedBy = performedBy;
+    }
+    
     
 }
