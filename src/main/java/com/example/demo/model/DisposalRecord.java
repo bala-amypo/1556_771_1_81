88a9b7e5a7c39c9c49d8995e8292d3f2c7cf4698
bala-amypo <bala@amypo.in>
2025-12-19 09:27:31 +0000

    User commit

--- src/main/java/com/example/demo/model/DisposalRecord.java
+++ src/main/java/com/example/demo/model/DisposalRecord.java
@@ -1,8 +1,17 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "disposal_records")
@@ -44,11 +53,76 @@ public class DisposalRecord {
         this.createdAt = createdAt;
     }
 
+    public DisposalRecord(User approvedBy, Asset asset, LocalDateTime createdAt, LocalDate disposalDate, String disposalMethod, String notes) {
+        this.approvedBy = approvedBy;
+        this.asset = asset;
+        this.createdAt = createdAt;
+        this.disposalDate = disposalDate;
+        this.disposalMethod = disposalMethod;
+        this.notes = notes;
+    }
+
     @PrePersist
     public void prePersist() {
         if (createdAt == null) {
             createdAt = LocalDateTime.now();
         }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Asset getAsset() {
+        return asset;
+    }
+
+    public String getDisposalMethod() {
+        return disposalMethod;
+    }
+
+    public LocalDate getDisposalDate() {
+        return disposalDate;
+    }
+
+    public User getApprovedBy() {
+        return approvedBy;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAsset(Asset asset) {
+        this.asset = asset;
+    }
+
+    public void setDisposalMethod(String disposalMethod) {
+        this.disposalMethod = disposalMethod;
+    }
+
+    public void setDisposalDate(LocalDate disposalDate) {
+        this.disposalDate = disposalDate;
+    }
+
+    public void setApprovedBy(User approvedBy) {
+        this.approvedBy = approvedBy;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 
 
